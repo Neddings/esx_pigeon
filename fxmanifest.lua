8411d333ccbd894fb3f6b0bbaf6f7504d19df53e
Neddings <github@neddings.com>
2021-07-06 21:52:03 +0200

    Initial push

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Neddings'
+
+description 'Pigeons are the new Phone'
+
+version '1.0.0'
+
+client_script 'client/main.lua'
+
+shared_script 'shared/config.lua'
+
+server_script 'server/main.lua'
